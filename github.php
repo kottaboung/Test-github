49219cddadd3b234a8adb55f2acaaf625aab36c2
Main Kottaboung <115133179+kottaboung@users.noreply.github.com>
2022-10-06 15:45:18 +0700

    Add files via upload
    upload

---
+++ github.php
@@ -0,0 +1,28 @@
+<html>
+    
+
+<style>
+     #blue_bar{
+            height:50px; background-color: black; color: #d9dfeb;
+        }
+
+
+</style>
+   
+    <body>
+            
+            <div id="blue_bar">
+                <div style="width:800px; margin:auto; font-size:30px;">
+                    kottaboung Nanvong<br><br>
+                    <img src="1.jpg" style=" width: 300px; height: 300px; 
+                            border:solid 2px white;">
+                </div>
+            </div>
+        </body> 
+        
+       
+   
+    <div>
+
+    </div>
+</html>
